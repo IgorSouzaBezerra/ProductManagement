ba516e559a4e2a7d80cdf8b177eea4fdcae21738
IgorSouzaBezerra <igor.souza1.bezerra@gmail.com>
2021-05-30 10:50:09 -0300

    (feat): alter documentation url

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
