f09c755ed1e760651fa3acee8d93015fadd1215f
IgorSouzaBezerra <igor.souza1.bezerra@gmail.com>
2021-05-30 10:30:44 -0300

    (feat): manager permission to list users

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 import { CreateUserController } from "../../../../modules/users/services/createUser/CreateUserController";
 import { ListUsersController } from "../../../../modules/users/services/listUsers/ListUsersController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureIsManager } from "../middlewares/ensurerIsManager";
 
 const userRoutes = Router();
 
@@ -9,6 +11,9 @@ const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
 
 userRoutes.post("/", createUserController.handle);
+
+userRoutes.use(ensureAuthenticated, ensureIsManager);
+
 userRoutes.get("/", listUsersController.handle);
 
 export { userRoutes };
